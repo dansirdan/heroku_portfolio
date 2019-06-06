87fcd51dd86da7aacde7d698fbdaf00de4f1d3d1
dansirdan <daniel.monteton@gmail.com>
2019-06-06 00:28:47 -0600

    commenting out unneccesary code for admin

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,30 +2,31 @@
 var path = require("path");
 //
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+// var isAuthenticated = require("../config/middleware/isAuthenticated");
 //
 module.exports = function (app) {
   //
   app.get("/", function (req, res) {
     // If the user already has an account send them to the admin page
-    if (req.user) {
-      res.redirect("/admin");
-    }
+    // if (req.user) {
+    //   res.redirect("/admin");
+    // }
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
   //
-  app.get("/login", function (req, res) {
-    // If the user already has an account send them to the admin page
-    if (req.user) {
-      res.redirect("/admin");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
+  // app.get("/login", function (req, res) {
+  //   // If the user already has an account send them to the admin page
+  //   if (req.user) {
+  //     res.redirect("/admin");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/login.html"));
+  // });
   //
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be 
   //redirected to the signup page
-  app.get("/admin", isAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/admin.html"));
-  });
+
+  // app.get("/admin", isAuthenticated, function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/admin.html"));
+  // });
 };
